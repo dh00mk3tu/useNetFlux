753dc3f4ccb157a883730454245767b437129cc0
dk3tm <dk3tm@dk3tm.dk3tm>
2024-11-25 01:28:29 +0530

    fixed issue with renderJsonPayloads

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: false },
+  experimental: {
+    renderJsonPayloads: false
+  },
   modules: ["@pinia/nuxt", "@nuxt/content", "@nuxt/image"],
   css: ["@/assets/css/tailwind.css", "@/assets/css/theme.css"],
   postcss: {
